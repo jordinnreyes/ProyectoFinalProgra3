ae82f208d4199310bde0d0bbcfbc8749e6fc1886
jordinnreyes <jordinn.reyes@utec.edu.pe>
2024-11-30 21:03:29 -0500

    carpeta pelicula

---
+++ classPelicula/pelicula.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jordinn on 20/11/2024.
+//
+
+#include "pelicula.h"
---
+++ classPelicula/pelicula.h
@@ -0,0 +1,16 @@
+//
+// Created by Jordinn on 20/11/2024.
+//
+
+#ifndef PELICULA_H
+#define PELICULA_H
+
+
+
+class pelicula {
+
+};
+
+
+
+#endif //PELICULA_H
